bdc3b3956a818fe8d90bf19b599cd05d2576c6b2
hussain <hussainwajda786@gmail.com>
2025-06-16 16:43:14 +0530

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I am  Passionate and results-driven Full-Stack Developer with a strong background in crafting intuitive UIs, integrating secure backend APIs, and optimizing performance
+          <br />
+          <br />
+          Skilled in cloud-based development with hands-on exposure to AWS services, containerization (Docker), and MongoDB
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/WhatIDo.tsx
@@ -0,0 +1,174 @@
+import { useEffect, useRef } from "react";
+import "./styles/WhatIDo.css";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const WhatIDo = () => {
+  const containerRef = useRef<(HTMLDivElement | null)[]>([]);
+  const setRef = (el: HTMLDivElement | null, index: number) => {
+    containerRef.current[index] = el;
+  };
+  useEffect(() => {
+    if (ScrollTrigger.isTouch) {
+      containerRef.current.forEach((container) => {
+        if (container) {
+          container.classList.remove("what-noTouch");
+          container.addEventListener("click", () => handleClick(container));
+        }
+      });
+    }
+    return () => {
+      containerRef.current.forEach((container) => {
+        if (container) {
+          container.removeEventListener("click", () => handleClick(container));
+        }
+      });
+    };
+  }, []);
+  return (
+    <div className="whatIDO">
+      <div className="what-box">
+        <h2 className="title">
+          W<span className="hat-h2">HAT</span>
+          <div>
+            I<span className="do-h2"> DO</span>
+          </div>
+        </h2>
+      </div>
+      <div className="what-box">
+        <div className="what-box-in">
+          <div className="what-border2">
+            <svg width="100%">
+              <line
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="100%"
+                stroke="white"
+                strokeWidth="2"
+                strokeDasharray="7,7"
+              />
+              <line
+                x1="100%"
+                y1="0"
+                x2="100%"
+                y2="100%"
+                stroke="white"
+                strokeWidth="2"
+                strokeDasharray="7,7"
+              />
+            </svg>
+          </div>
+          <div
+            className="what-content what-noTouch"
+            ref={(el) => setRef(el, 0)}
+          >
+            <div className="what-border1">
+              <svg height="100%">
+                <line
+                  x1="0"
+                  y1="0"
+                  x2="100%"
+                  y2="0"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeDasharray="6,6"
+                />
+                <line
+                  x1="0"
+                  y1="100%"
+                  x2="100%"
+                  y2="100%"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeDasharray="6,6"
+                />
+              </svg>
+            </div>
+            <div className="what-corner"></div>
+
+            <div className="what-content-in">
+              <h3>DEVELOP</h3>
+              <h4>Description</h4>
+              <p>
+                From Starting with HTML, CSS, and JavaScript to building complex MERN Stack applications, I've honed my skills in Full-Stack development.
+                <br />
+                <br />
+              </p>
+              <h5>Skillset & tools</h5>
+              <div className="what-content-flex">
+                <div className="what-tags">JavaScript</div>
+                <div className="what-tags">TypeScript</div>
+                <div className="what-tags">React</div>
+                <div className="what-tags">Tailwind Css</div>
+                <div className="what-tags">Node.js</div>
+                <div className="what-tags">Next.js</div>
+                <div className="what-tags">Express.js</div>
+                <div className="what-tags">Python</div>
+                <div className="what-tags">Django</div>
+                <div className="what-tags">MongoDB</div>
+                <div className="what-tags">MySql</div>
+              </div>
+              <div className="what-arrow"></div>
+            </div>
+          </div>
+          <div
+            className="what-content what-noTouch"
+            ref={(el) => setRef(el, 1)}
+          >
+            <div className="what-border1">
+              <svg height="100%">
+                <line
+                  x1="0"
+                  y1="100%"
+                  x2="100%"
+                  y2="100%"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeDasharray="6,6"
+                />
+              </svg>
+            </div>
+            <div className="what-corner"></div>
+            <div className="what-content-in">
+              <h3>DevOps</h3>
+              <h4>Description</h4>
+              <p>
+                I started DevOps as a curiosity, captivated by its promise of efficiency and collaboration. Now, it's not just a job; it's the rhythm of how I build and deliver.
+                <br />
+                <br />
+              </p>
+              <h5>Skillset & Technologies</h5>
+              <div className="what-content-flex">
+                <div className="what-tags">Docker</div>
+                <div className="what-tags">Containerization</div>
+                <div className="what-tags">AWS</div>
+                <div className="what-tags">Kubernetes</div>
+                <div className="what-tags">Nginx</div>
+                <div className="what-tags">Caddy</div>
+                <div className="what-tags">AWS ECS</div>
+              </div>
+              <div className="what-arrow"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhatIDo;
+
+function handleClick(container: HTMLDivElement) {
+  container.classList.toggle("what-content-active");
+  container.classList.remove("what-sibling");
+  if (container.parentElement) {
+    const siblings = Array.from(container.parentElement.children);
+
+    siblings.forEach((sibling) => {
+      if (sibling !== container) {
+        sibling.classList.remove("what-content-active");
+        sibling.classList.toggle("what-sibling");
+      }
+    });
+  }
+}
